59ff3f6f9e7f00f46ea90a6641d0496db42144c5
Denis Kiselyov <denis.kiselyov@gmail.com>
2024-07-06 19:01:03 +0700

    Assemble the whole workflow and update tests

--- PhonePadTranslation.Tests/PadTranslatorTests.cs
+++ PhonePadTranslation.Tests/PadTranslatorTests.cs
@@ -8,6 +8,9 @@ namespace PhonePadTranslation.Tests
         private Mock<IPadValidator> inputValidator;
         private Mock<IPreprocessor> preprocessor;
         private Mock<IParser> parser;
+        private Mock<IDictionary> dictionary;
+
+        private string input;
 
         [SetUp]
         public void Setup()
@@ -15,24 +18,29 @@ namespace PhonePadTranslation.Tests
             preprocessor = new Mock<IPreprocessor>();
             inputValidator = new Mock<IPadValidator>();
             parser = new Mock<IParser>();
+            dictionary = new Mock<IDictionary>();
 
-            subject = new PadTranslator(preprocessor.Object, inputValidator.Object, parser.Object);
+            input = "222#";
+
+            var sequence = new MockSequence();
+            preprocessor.InSequence(sequence).Setup(p => p.Preprocess(input)).Returns(input);
+            inputValidator.InSequence(sequence).Setup(v => v.ValidatePadInput(input));
+            parser.InSequence(sequence).Setup(p => p.Parse(input)).Returns(new List<Tuple<char, int>>() { new('2', 3) });
+            dictionary.InSequence(sequence).Setup(d => d.Translate(It.IsAny<char>(), It.IsAny<int>())).Returns('C');
+
+            subject = new PadTranslator(preprocessor.Object, inputValidator.Object, parser.Object, dictionary.Object);
         }
 
         #region Workflow
         [Test]
         public void OldPhonePad_ShouldCallMethodsInCorrectOrder()
         {
-            string input = "222#";
-
             subject.OldPhonePad(input);
-
-            var sequence = new MockSequence();
-            preprocessor.InSequence(sequence).Setup(p => p.Preprocess(input)).Returns(input);
-            inputValidator.InSequence(sequence).Setup(v => v.ValidatePadInput(input));
 
             preprocessor.Verify(p => p.Preprocess(input), Times.Once);
             inputValidator.Verify(validator => validator.ValidatePadInput(It.IsAny<string>()), Times.Once);
+            parser.Verify(p => p.Parse(It.IsAny<string>()), Times.Once);
+            dictionary.Verify(d => d.Translate(It.IsAny<char>(), It.IsAny<int>()), Times.AtLeastOnce);
         }
         #endregion
 
@@ -44,10 +52,43 @@ namespace PhonePadTranslation.Tests
         }
 
         [Test]
-        [Ignore("This test is not implemented yet")]
         public void OldPhonePad_TranslatesMultipleChars()
         {
-            Assert.That(subject.OldPhonePad("2345#"), Is.EqualTo("ADGJ"));
+            input = "2345#";
+            preprocessor.Setup(p => p.Preprocess(input)).Returns(input);
+            parser.Setup(p => p.Parse(input)).Returns(new List<Tuple<char, int>>() { new('2', 1), new('3', 1), new('4', 1), new('5', 1) });
+            dictionary.Setup(d => d.Translate('2', 1)).Returns('A');
+            dictionary.Setup(d => d.Translate('3', 1)).Returns('D');
+            dictionary.Setup(d => d.Translate('4', 1)).Returns('G');
+            dictionary.Setup(d => d.Translate('5', 1)).Returns('J');
+
+            Assert.That(subject.OldPhonePad(input), Is.EqualTo("ADGJ"));
+        }
+
+        [Test]
+        [Ignore("Skip due to stabbed functionality")]
+        public void OldPhonePad_GivenTest1()
+        {
+            Assert.That(subject.OldPhonePad("33#"), Is.EqualTo("E"));
+        }
+
+        [Test]
+        [Ignore("Skip due to stabbed functionality")]
+        public void OldPhonePad_GivenTest2() {
+            Assert.That(subject.OldPhonePad("227*#"), Is.EqualTo("B"));
+        }
+
+        [Test]
+        [Ignore("Skip due to stabbed functionality")]
+        public void OldPhonePad_GivenTest3() {
+            Assert.That(subject.OldPhonePad("4433555 555666#"), Is.EqualTo("HELLO"));
+        }
+
+        [Test]
+        [Ignore("Skip due to stabbed functionality")]
+        public void OldPhonePad_GivenTest4()
+        {
+            Assert.That(subject.OldPhonePad("8 88777444666*664#"), Is.EqualTo("TURING"));
         }
         #endregion
     }
--- PhonePadTranslation/PadTranslator.cs
+++ PhonePadTranslation/PadTranslator.cs
@@ -1,26 +1,37 @@
-﻿namespace PhonePadTranslation
+﻿using System.Text;
+
+namespace PhonePadTranslation
 {
     public class PadTranslator
     {
         private readonly IPreprocessor preprocessor;
         private readonly IPadValidator inputValidator;
         private readonly IParser parser;
+        private readonly IDictionary dictionary;
 
-        public PadTranslator(IPreprocessor preprocessor, IPadValidator inputValidator, IParser parser)
+        public PadTranslator(IPreprocessor preprocessor, IPadValidator inputValidator, IParser parser, IDictionary dictionary)
         {
             this.preprocessor = preprocessor;
             this.inputValidator = inputValidator;
             this.parser = parser;
+            this.dictionary = dictionary;
         }
 
         public String OldPhonePad(String input)
         {
+            StringBuilder result = new StringBuilder();
+
             var prerpocessedInput = preprocessor.Preprocess(input);
             inputValidator.ValidatePadInput(prerpocessedInput);
 
             var parsedInput = parser.Parse(prerpocessedInput);
 
-            return "C";
+            parsedInput.ForEach(tuple =>
+            {
+                result.Append(dictionary.Translate(tuple.Item1, tuple.Item2));
+            });
+
+            return result.ToString();
         }
     }
 }
